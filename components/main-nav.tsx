a2efb8d63377dda99d066191076a1a52380486b3
Plopmenz <plopmenz@gmail.com>
2024-01-10 18:18:05 +0100

    cleanup

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,10 +1,16 @@
 import * as React from "react"
 import Link from "next/link"
 
-import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
+
+interface NavItem {
+  title: string
+  href?: string
+  disabled?: boolean
+  external?: boolean
+}
 
 interface MainNavProps {
   items?: NavItem[]
